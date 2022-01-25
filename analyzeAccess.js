fd62ec184d41449752d20060026cf4f3770a711d
CarlosMendesDev <carlos.henrique8@hotmail.com>
2022-01-25 12:49:55 -0300

    feat(api): search number of hits
    - Adds function to search number of hits;

---
+++ analyzeAccess.js
@@ -0,0 +1,33 @@
+'use strict';
+const countapi = require('countapi-js');
+
+module.exports.getAcessNumber = async (event) => {
+  const namespace = 'ton.com.br';
+
+  const data = await countapi.get(namespace, 'visits');
+
+  if(data.status === 200) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: `Número de acessos em ${namespace}`,
+          ...data,
+        },
+        null,
+        2
+      ),
+    };
+  } else {
+    return {
+      statusCode: data.status,
+      body: JSON.stringify(
+        {
+          message: `Error ${data.status}`,
+        },
+        null,
+        2
+      ),
+    };
+  }
+};
