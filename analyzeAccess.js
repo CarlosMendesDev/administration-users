5969b714fbd03f886e29711d26228e6af1521653
CarlosMendesDev <carlos.henrique8@hotmail.com>
2022-01-25 12:52:40 -0300

    feat(api): increment number of hits
    - Adds function to increment number of hits;

--- analyzeAccess.js
+++ analyzeAccess.js
@@ -31,3 +31,34 @@ module.exports.getAcessNumber = async (event) => {
     };
   }
 };
+
+module.exports.incrementAcessNumber = async (event) => {
+  const namespace = 'ton.com.br';
+
+  const data = await countapi.hit(namespace, 'visits');
+
+  if(data.status === 200) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: `Número de acessos em ${namespace}`,
+          ...data,
+        },
+        null,
+        2
+      ),
+    };
+  } else {
+    return {
+      statusCode: data.status,
+      body: JSON.stringify(
+        {
+          message: `Error ${data.status}`,
+        },
+        null,
+        2
+      ),
+    };
+  }
+};
